e5c98dde0952eba6d5ba08a321543e3f37b2296a
John Noss <john.noss@asics.com>
2019-03-20 16:35:11 -0400

    feat: add output containing example curl command

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "x_manual_auth_target_string" {
   description = "Secret that this WAF will check for in x-manual-auth header"
   sensitive   = true
 }
+
+output "test_authentication_curl_command" {
+  value = "curl -H \'x-manual-auth: ${local.x_manual_auth_secret_target_string}\' https://<your application hostname here>"
+}
